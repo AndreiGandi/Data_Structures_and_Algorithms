e1f1540d7aa118b3792bb9705f13b84e40310012
Andrei Gindilă <gandilaandrei@gmail.com>
2020-10-25 15:23:22 +0200

    Stack

---
+++ Stack/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Stack
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Stack cars = new Stack(10);
+            cars.Push("Volvo");
+            cars.Push("BMW");
+            cars.Push("Citroen");
+            cars.Push("Mercedes");
+            cars.Push("Jeep");
+
+            cars.DisplayStack();
+
+            Console.WriteLine(cars.Peek());
+
+            cars.Pop();
+
+            cars.DisplayStack();
+            Console.WriteLine(cars.Peek());
+
+
+
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private int size; // Maximum Stack's size
+        private string[] stackArray;
+        private int top; //index of stack's last element
+
+        public Stack(int size)
+        {
+            this.size = size;
+            stackArray = new string[size];
+            top = -1;
+        }
+
+        public void Push(string element)
+        {
+            if(isFull())
+            {
+                Console.WriteLine("The Stack is Full.");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = element;
+            }
+        }
+        public string Pop()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("The Stack is Empty");
+                return "--";
+            }
+            else
+            {
+                int old_top = top;
+                top--;
+                //stackArray[old_top] = "Empty Slot";
+                return stackArray[old_top];
+            }
+
+        }
+
+        public string Peek()
+        {
+            return stackArray[top];
+        }
+
+        public void DisplayStack()
+        {
+            for(int i = stackArray.Length-1; i>=0; i--)
+            {
+                if(stackArray[i] != null)
+                Console.WriteLine("["+stackArray[i]+"]");
+                else Console.WriteLine("[Empty Slot]");
+
+            }
+            Console.WriteLine();
+        }
+
+        public bool isEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool isFull()
+        {
+            return (top == size - 1);
+        }
+
+
+    }
+}
