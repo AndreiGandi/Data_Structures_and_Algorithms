f0bd971a22568cf4dabc8dd7c3938f2af086695e
Andrei Gindilă <gandilaandrei@gmail.com>
2020-10-25 20:17:28 +0200

    LinearSearch

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the algorithm is very inefficient for large amounts of data
+            int[] arr = { 1, 5, 2, 4, 8, 9, 12, 33, 456, 123, 22, 19 };
+            int search = 19;
+            Console.WriteLine($"{search} is located at index {LinearSearch(arr, search)}");
+        }
+        public static int LinearSearch(int[] arr, int elem)
+        {
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if (arr[i] == elem) return i;
+            }
+            return -1;
+        }
+    }
+}
