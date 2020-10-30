bf28ea41019b9b73587f87295c816a510fe663ac
Andrei Gindilă <gandilaandrei@gmail.com>
2020-10-30 23:39:41 +0200

    Recursion

---
+++ Factorial/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        //n! = 1 * 2 * 3 * ... * n
+        //0! = 1
+        static void Main(string[] args)
+        {
+            int nr = 5; //120
+            //Console.WriteLine($"{nr}! = {Factorial(nr)}");
+            Console.WriteLine($"{nr}! = {RecursiveFactorial(nr)}");
+        }
+
+        //Iterative method
+        static long Factorial(int nr)
+        {
+            long Fact = 1;
+            for(int i = 2; i<=nr; i++)
+            {
+                Fact *= i;
+            }
+            return Fact;
+        }
+
+        //Recursive method
+        static long RecursiveFactorial(int nr)
+        {
+            if (nr == 0) return 1;
+            else return nr * RecursiveFactorial(nr - 1);
+        }
+
+        //static BigInteger RecursiveFactorial(int nr)
+        //{
+        //    if (nr == 0) return 1;
+        //    else return nr * RecursiveFactorial(nr - 1);
+        //}
+    }
+}
---
+++ Fibonacci/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class EntryPoint
+    {
+        //Fibonacci : 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+        static void Main(string[] args)
+        {
+            int nr = 3;
+            Console.WriteLine($"Firsts {nr} elements of Fibonacci's serie\n");
+            Console.WriteLine("*************************Iterative methods*************************");
+            Console.WriteLine(Fibonacci(nr));
+            Console.WriteLine("*************************Recursive methods*************************");
+            Console.WriteLine(RecursiveFibonacci(nr));
+        }
+
+        //Iterative method
+        static string Fibonacci(int n)
+        {
+            long last = 1;
+            long secondToLast = 0;
+            long Fibo = 0;
+            string sFibo = "0 1 ";
+            for(int i = 3; i<=n; i++)
+            {
+                Fibo = secondToLast + last;
+                secondToLast = last;
+                last = Fibo;
+                sFibo += Fibo.ToString()+" ";
+            }
+            return sFibo;
+        }
+
+        //Recursive method
+        static long RecursiveFibonacci(int n)
+        {
+            if (n <= 3) return 1;
+            else return RecursiveFibonacci(n-1) + RecursiveFibonacci(n - 2);
+        }
+    }
+}
