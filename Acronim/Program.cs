79ffd77360cdfd2553fac1cd5f28e76561dab3aa
Andrei Gindilă <gandilaandrei@gmail.com>
2020-11-13 00:03:57 +0200

    Exercism

---
+++ Acronim/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Acronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Abbreviate("Portable Network Graphics")); //PNG
+            Console.WriteLine(Abbreviate("Ruby on Rails")); //ROR
+            Console.WriteLine(Abbreviate("First In, First Out")); //FIFO
+            Console.WriteLine(Abbreviate("GNU Image Manipulation Program")); //GIMP
+            Console.WriteLine(Abbreviate("Complementary metal-oxide semiconductor"));//CMOS
+            Console.WriteLine(Abbreviate("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me")); // ROTFLSHTMDCOALM
+            Console.WriteLine(Abbreviate("Something - I made up from thin air")); //SIMUFTA
+            Console.WriteLine(Abbreviate("Halley's Comet")); //HC
+            Console.WriteLine(Abbreviate("The Road _Not_ Taken")); //TRNT
+
+        }
+        public static string Abbreviate(string phrase)
+        {
+            string[] words = phrase.Split(new Char[] { ' ', '-' });
+            StringBuilder sb = new StringBuilder();
+            foreach(var word in words)
+            {
+                sb.Append(GetFirstUpperLetter(word));
+            }
+            return sb.ToString();
+        }
+        public static string GetFirstUpperLetter(string word)
+        {
+
+            foreach(var ch in word)
+            {
+                if(char.IsLetter(ch))
+                    return ch.ToString().ToUpper();
+            }
+            return "";
+            
+        }
+
+    }
+}
---
+++ Acronim/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Acronim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5280afb2-5a39-4787-a174-2efd09875660")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion(Abbreviate(Abbreviate("1.0.*Abbreviate(Abbreviate(")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
