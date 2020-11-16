1a62273823de84a0a8de60f70d1854eb9f6c795f
Andrei Gindilă <gandilaandrei@gmail.com>
2020-11-16 15:44:44 +0200

    Project Euler 6

---
+++ Problem 6 Sum square difference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6_Sum_square_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SquareOfSum(100) - SumOfSquares(100));
+        }
+        public static long SumOfSquares(int limit)
+        {
+            long Sum = 0;
+            for(int i=1; i<=limit; i++)
+            {
+                Sum += i * i;
+            }
+            return Sum;
+        }
+        public static long SquareOfSum(int limit)
+        {
+            long Sum = 0;
+            Sum = limit * (limit + 1) / 2;
+
+            return Sum * Sum;
+        }
+
+    }
+}
