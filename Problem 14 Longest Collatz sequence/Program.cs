255cc65b45987b61f54d2b1cae094d99188e1f7d
Andrei Gindilă <gandilaandrei@gmail.com>
2020-11-18 18:50:47 +0200

    Project Euler 14

---
+++ Problem 14 Longest Collatz sequence/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_14_Longest_Collatz_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int limit = 1000000;
+
+            long max = 0;
+            long number = 0;
+            long sequence;
+
+            for (int i = 2; i <= limit; i++)
+            {
+                int length = 1;
+                sequence = i;
+                while (sequence != 1)
+                {
+                    if ((sequence % 2) == 0)
+                    {
+                        sequence = sequence / 2;
+                    }
+                    else
+                    {
+                        sequence = sequence * 3 + 1;
+                    }
+                    length++;
+                }
+                if (length > max)
+                {
+                    max = length;
+                    number = i;
+                }
+            }
+            Console.WriteLine(number);
+        }
+        //public static int Collatz(int nr)
+        //{
+        //    int steps = 1;
+        //    while(nr!=1)
+        //    {
+        //        if (nr % 2 == 0) nr /= 2;
+        //        else nr = 3 * nr + 1;
+        //        steps++;
+        //    }
+        //    return steps;
+        //}
+    }
+}
---
+++ Problem 14 Longest Collatz sequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Problem 14 Longest Collatz sequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Problem 14 Longest Collatz sequence")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ecc49540-82a2-4b04-8b97-a7d5faf48a63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build limit
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision limits
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
